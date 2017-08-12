a5cc67e52795093bad319396e41c3debced0a5d5
Yana Agun Siswanto <bekicot@users.noreply.github.com>
2017-08-12 10:03:09 +0700

    Update articles_controller.rb
    Fix Indentation

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,16 +1,16 @@
 module Api
-	module V1
-		class ArticlesController < ApplicationController
-			protect_from_forgery prepend: true, with: :exception
-			def index
-				articles = Article.order('created_at DESC');
-				puts render json: {status: 'hoho Sukses', message: 'Loader article', data:articles}, status: :ok
-			end
-			def show
-				article = Article.find(params[:id])
-				puts render json: {status: 'hoho Sukses', message: 'Loader article', data:article}, status: :ok
-			end
-			def create
+  module V1
+    class ArticlesController < ApplicationController
+      protect_from_forgery prepend: true, with: :exception
+      def index
+        articles = Article.order('created_at DESC');
+        puts render json: {status: 'hoho Sukses', message: 'Loader article', data:articles}, status: :ok
+      end
+      def show
+        article = Article.find(params[:id])
+        puts render json: {status: 'hoho Sukses', message: 'Loader article', data:article}, status: :ok
+      end
+      def create
         article = Article.new(article_params)
         if article.save
           puts render json: {status: 'SUCCESS', message:'Saved article', data:article},status: :ok
@@ -18,10 +18,10 @@ module Api
           puts render json: {status: 'ERROR', message:'Article not saved', data:article.errors},status: :unprocessable_entity
         end
       end
-			private
-			def article_params
-	      params.require(:article).permit(:title, :body)
-	    end
-		end
-	end
+      private
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
+    end
+  end
 end
